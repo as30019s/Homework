ab231c46cdb3171ce3fef3dd6e789530f71e7b7b
as30019s <as30019s@gmail.com>
2023-06-01 16:08:34 +0800

    #update

--- Homework/Homework_09.cs
+++ Homework/Homework_09.cs
@@ -23,180 +23,60 @@ namespace Homework
 
         private void btn_OX_00_Click(object sender, EventArgs e)
         {
-
-            if (currentUserCheck)
-            {
-                btn_OX_00.Text = "〇";
-                layout[0, 0] = "〇";
-            }
-            else
-            {
-                btn_OX_00.Text = "X";
-                layout[0, 0] = "X";
-            }
-            btn_OX_00.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(0,0);
-            winnerMessage(result, layout[0, 0]);
+            updateMethod(btn_OX_00, 0, 0);
 
         }
 
         private void btn_OX_10_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_10.Text = "〇";
-                layout[1, 0] = "〇";
-            }
-            else
-            {
-                btn_OX_10.Text = "X";
-                layout[1, 0] = "X";
-            }
-            btn_OX_10.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(1, 0);
-            winnerMessage(result, layout[1, 0]);
-
+            updateMethod(btn_OX_10, 1,0);
         }
 
         private void btn_OX_20_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_20.Text = "〇";
-                layout[2, 0] = "〇";
-            }
-            else
-            {
-                btn_OX_20.Text = "X";
-                layout[2, 0] = "X";
-            }
-            btn_OX_20.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(2, 0);
-            winnerMessage(result, layout[2, 0]);
+            updateMethod(btn_OX_20, 2, 0);
 
         }
 
         private void btn_OX_01_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_01.Text = "〇";
-                layout[0, 1] = "〇";
-            }
-            else
-            {
-                btn_OX_01.Text = "X";
-                layout[0, 1] = "X";
-            }
-            btn_OX_01.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(0, 1);
-            winnerMessage(result, layout[0, 1]);
-
+            updateMethod(btn_OX_01, 0, 1);
         }
 
         private void btn_OX_11_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_11.Text = "〇";
-                layout[1, 1] = "〇";
-            }
-            else
-            {
-                btn_OX_11.Text = "X";
-                layout[1, 1] = "X";
-            }
-            btn_OX_11.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(1, 1);
-            winnerMessage(result, layout[1, 1]);
-
+            updateMethod(btn_OX_11, 1, 1);
         }
 
         private void btn_OX_21_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_21.Text = "〇";
-                layout[2, 1] = "〇";
-            }
-            else
-            {
-                btn_OX_21.Text = "X";
-                layout[2, 1] = "X";
-            }
-            btn_OX_21.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(2, 1);
-            winnerMessage(result, layout[2, 1]);
+            updateMethod(btn_OX_21, 2, 1);
 
         }
 
         private void btn_OX_02_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_02.Text = "〇";
-                layout[0, 2] = "〇";
-            }
-            else
-            {
-                btn_OX_02.Text = "X";
-                layout[0, 2] = "X";
-            }
-            btn_OX_02.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(0, 2);
-            winnerMessage(result, layout[0, 2]);
+            updateMethod(btn_OX_02, 0, 2);
 
         }
 
         private void btn_OX_12_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_12.Text = "〇";
-                layout[1, 2] = "〇";
-            }
-            else
-            {
-                btn_OX_12.Text = "X";
-                layout[1, 2] = "X";
-            }
-            btn_OX_12.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(1, 2);
-            winnerMessage(result, layout[1, 2]);
+            updateMethod(btn_OX_12, 1, 2);
 
         }
 
         private void btn_OX_22_Click(object sender, EventArgs e)
         {
-            if (currentUserCheck)
-            {
-                btn_OX_22.Text = "〇";
-                layout[2, 2] = "〇";
-            }
-            else
-            {
-                btn_OX_22.Text = "X";
-                layout[2, 2] = "X";
-            }
-            btn_OX_22.Enabled = false;
-            currentUserCheck = !currentUserCheck;
-            result = checkGameResult(2, 2);
-            winnerMessage(result, layout[2, 2]);
-
+            updateMethod(btn_OX_22, 2, 2);
         }
         public bool checkGameResult(int x, int y)
         {
             int countRow = 0;
             int countCol = 0;
             int countSlash = 0;
+            int countSlashReverse = 0;
+
             if (x == 0 && y == 0)
             {
                 for (int i = 0; i < 3; i++)
@@ -266,12 +146,87 @@ namespace Homework
                         countSlash++;
                     }
                 }
+            } else if (x == 1 && y == 0 )
+            {
+                for (int i = x-1;i<=2;i++)
+                {
+                    if (layout[i,y] == layout[x,y])
+                    {
+                        countRow++;
+                    }
+                    if (layout[x,i] == layout[x,y])
+                    {
+                        countCol++;
+                    }
+                }
+            } else if (x == 1 && y == 2)
+            {
+                for (int i = x - 1; i <= 2; i++)
+                {
+                    if (layout[i, y] == layout[x, y])
+                    {
+                        countRow++;
+                    }
+                }
+                for (int i = 2; i >= 0;i--)
+                {
+                    if (layout[x, i] == layout[x, y])
+                    {
+                        countCol++;
+                    }
+                }
+            } else if (x == 0 && y == 1)
+            {
+                for (int i = y - 1; i <= 2; i++)
+                {
+                    if (layout[i, y] == layout[x, y])
+                    {
+                        countRow++;
+                    }
+                    if (layout[x, i] == layout[x, y])
+                    {
+                        countCol++;
+                    }
+                }
+            } else if ( x == 2 && y == 1)
+            {
+                for (int i = y - 1; i <= 2; i++)
+                {
+                    if (layout[i, y] == layout[x, y])
+                    {
+                        countRow++;
+                    }
+                }
+                for (int i = 2; i >= 0; i--)
+                {
+                    if (layout[x, i] == layout[x, y])
+                    {
+                        countCol++;
+                    }
+                }
+            } else if (x==1 && y ==1)
+            {
+                for (int i = 0; i <= 2; i++)
+                {
+                    if (layout[i, y] == layout[x, y])
+                    {
+                        countRow++;
+                    }
+                    if (layout[x, i] == layout[x, y])
+                    {
+                        countCol++;
+                    }
+                    if (layout[i, i] == layout[x, y])
+                    {
+                        countSlash++;
+                    }
+                    if (layout[i, 2 - i] == layout[x, y])
+                    {
+                        countSlashReverse++;
+                    }
+                }
             }
-
-
-
-
-            if (countRow == 3 || countCol == 3 || countSlash == 3)
+            if (countRow == 3 || countCol == 3 || countSlash == 3 || countSlashReverse == 3)
             {
                 return true;
 
@@ -283,11 +238,33 @@ namespace Homework
 
         }
 
+        void updateMethod(Button button, int x, int y) 
+        {
+            if (currentUserCheck)
+            {
+                button.Text = "〇";
+                layout[x, y] = "〇";
+            }
+            else
+            {
+                button.Text = "X";
+                layout[x, y] = "X";
+            }
+            button.Enabled = false;
+            currentUserCheck = !currentUserCheck;
+            result = checkGameResult(x, y);
+            winnerMessage(result, layout[x, y]);
+        }
+
         void winnerMessage(bool boolResult, string user)
         {
             if (boolResult)
             {
                 MessageBox.Show($"{user}獲勝");
+                currentUserCheck = true;
+                result = false;
+                Controls.Clear();
+                InitializeComponent();
             }
         }
     }
